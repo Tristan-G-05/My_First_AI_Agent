0cbdce4e341dbd54c39b8a173e97d1186e74c492
Tristan-G-05 <Tristan.Garrison.C05@gmail.com>
2026-02-02 18:12:42 -0500

    Implement main functionality to load environment variables and generate content using the Gemini API

--- My_First_AI_Agent/main.py
+++ My_First_AI_Agent/main.py
@@ -1,5 +1,26 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from my-first-ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY not found in environment variables.")
+    
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+    if not response.usage_metadata:
+        raise RuntimeError("No usage metadata found in the response.")
+    Prompt_Tokens = response.usage_metadata.prompt_token_count
+    Response_Tokens = response.usage_metadata.candidates_token_count    
+    print(f"Prompt tokens: {Prompt_Tokens}")
+    print(f"Response tokens: {Response_Tokens}")
+    print(response.text)
 
 
 if __name__ == "__main__":
