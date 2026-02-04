8870cf04943cf4b7866e33f27a62a8dbe3db5ef4
Tristan-G-05 <Tristan.Garrison.C05@gmail.com>
2026-02-04 17:05:05 -0500

    Refactor main function to accept user input and enable verbose output for response details

--- My_First_AI_Agent/main.py
+++ My_First_AI_Agent/main.py
@@ -1,26 +1,44 @@
 import os
+import argparse
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    
+    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY not found in environment variables.")
     
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    
     client = genai.Client(api_key=api_key)
-
     response = client.models.generate_content(
     model='gemini-2.5-flash',
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+    contents = messages
+    )
+    
     if not response.usage_metadata:
-        raise RuntimeError("No usage metadata found in the response.")
-    Prompt_Tokens = response.usage_metadata.prompt_token_count
-    Response_Tokens = response.usage_metadata.candidates_token_count    
-    print(f"Prompt tokens: {Prompt_Tokens}")
-    print(f"Response tokens: {Response_Tokens}")
-    print(response.text)
+        raise RuntimeError("No usage metadata found in the response.")  
+    
+
+    if args.verbose:
+        print("User prompt:", args.user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count  )
+        print("Response:")
+        print(response.text)
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
