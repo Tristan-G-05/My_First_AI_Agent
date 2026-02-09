82148ccaa7846976a84e5d9cf8c295a31f8ba811
Tristan-G-05 <Tristan.Garrison.C05@gmail.com>
2026-02-09 17:36:52 -0500

    Add system prompt for chatbot response behavior and integrate into content generation

--- My_First_AI_Agent/main.py
+++ My_First_AI_Agent/main.py
@@ -4,6 +4,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 
 
@@ -24,7 +25,8 @@ def main():
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
     model='gemini-2.5-flash',
-    contents = messages
+    contents = messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0,),
     )
     
     if not response.usage_metadata:
